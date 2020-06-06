13de8acf916288d933c89f4ffb5d18b7bce1316d
Rocky Gray <rocky.grayjr@gmail.com>
2020-06-05 23:22:32 -0400

    add request logging for file server

--- cmd/fileserver/main.go
+++ cmd/fileserver/main.go
@@ -13,5 +13,14 @@ func main() {
 	port := 9090
 	log.Printf("serving %s on port %d\n", *filepath, port)
 	fs := http.FileServer(http.Dir(*filepath))
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), fs))
+	// if accessing from a container to the host, then use the following URL template
+	// http://host.docker.internal:9090/${pathToFile}
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), logRequestHandler(fs)))
+}
+
+func logRequestHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		h.ServeHTTP(w, r)
+	})
 }
